4abcc8bf328d3f82136915944e206ac139bf8985
belaidseif <belaidseif@gmail.com>
2020-06-15 14:06:32 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListStudentComponent } from './student/list-student/list-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListStudentComponent
   ],
   imports: [
     BrowserModule
